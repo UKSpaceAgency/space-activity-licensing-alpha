43268255df57c77c303fcc40b331e7ef4e4a105b
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-03-20 16:45:42 +0000

    Splitting list into two sections, visual notification update

--- app/shared/components/PageTwoCol/component.jsx
+++ app/shared/components/PageTwoCol/component.jsx
@@ -6,11 +6,15 @@ import Hero from '../Hero/component.jsx'
 import Grid from '../Grid/component.jsx'
 import GridCol from '../GridCol/component.jsx'
 import OverviewList from '../OverviewList/component.jsx'
+import Heading from '../Heading/component.jsx'
 import FilterBlock from '../../containers/FilterBlock/component.jsx'
 
 const PageTwoCol = props => {
   let list = props.listing.map((v, i) => {
-    return <OverviewList {...v} key={i}/>
+    return (<div>
+      <Heading {...v.heading}/>
+      {v.list.map((j, i) => <OverviewList {...j} key={i}/>)}
+    </div>)
   })
 
   return (
