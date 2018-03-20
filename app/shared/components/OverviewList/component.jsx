9aa758ccffebf628ff10c0655d73aa12092c4b00
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-03-20 10:51:48 +0000

    Overlisting block - amends to update design change

--- app/shared/components/OverviewList/component.jsx
+++ app/shared/components/OverviewList/component.jsx
@@ -5,27 +5,31 @@ import Heading from '../Heading/component.jsx'
 import classNames from 'classnames'
 
 const OverviewList = props => {
-  let classes = classNames('bg-grey spacing-bottom--single', props.className, props.modifiers)
+  let classes = classNames('bg-grey spacing-bottom--single', props.className, props.modifiers, 'relative bottom-border bottom-border-' + props.label.class)
 
   return (
     <section className={classes}>
       <Grid>
         <GridCol className='column-half'>
           <div className='padding-medium'>
-            <a href={props.url}>{props.status && <span className='badge bg-black'>{props.status} </span>}<Heading {...props.heading}/></a>
-            <p>{props.name}: {props.applicantName}</p>
-            <span className={'badge badge-fixed ' + props.label.class}>{props.label.tag}</span>
+            <a href={props.url}><h3 className='heading-medium spacing-top--flush'>{props.status && <span className='badge bg-black'>{props.status} </span>} {props.heading.text}</h3></a>
+            <p><b>{props.name}</b>: {props.applicantName}</p>
           </div>
         </GridCol>
         <GridCol className='column-quarter'>
-          <p>Last updated<br/>{props.updated}</p>
-          <p>By: {props.updater}</p>
+          <div className='padding-medium'>
+            <p>Last updated<br/><b>{props.updated}</b></p>
+            <p>By: <b>{props.updater}</b></p>
+          </div>
         </GridCol>
         <GridCol className='column-quarter'>
-          <p>Application status</p>
-          {props.appStatus}
+          <div className='padding-medium'>
+            <p>Application status</p>
+            <b>{props.appStatus}</b>
+          </div>
         </GridCol>
       </Grid>
+      <span className={'badge badge--fixed ' + props.label.class}>{props.label.tag}</span>
     </section>
   )
 }
