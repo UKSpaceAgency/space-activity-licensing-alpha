394b30e5fea751e4f01a4cfc78cc35e9f57bd073
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-03-27 13:57:18 +0100

    Linting to remove extra space

--- app/shared/containers/SidenavContainer/component.jsx
+++ app/shared/containers/SidenavContainer/component.jsx
@@ -7,7 +7,6 @@ const SidenavContainer = props => {
   // quick hygiene thing to ensure React hasn't done something weird
   let flattened = isArray(props.content) ? props.content : Object.values(props.content)
   let list = flattened.map((v, i) => {
-
     return {
       'text': v.title.text,
       'url': '#' + v.permalink,
