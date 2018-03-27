f767aca153d1d2830122527f6274dfeb6fdcd4af
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-03-27 17:13:25 +0100

    List change - shift the left padding to a .spaced class

--- app/shared/components/DateInput/component.jsx
+++ app/shared/components/DateInput/component.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import classNames from 'classnames'
 
 const DateInput = props => {
-  let classes = classNames('list', props.className, props.modifiers)
-
   return (
     <div className='form-group'>
-      <Fieldset />
+      <fieldset>
+        <legend>
+          <span className='form-label-bold'>
+            What is your date of birth?
+          </span>
+          <span className='form-hint'>For example, 31 3 1980</span>
+        </legend>
+        <div className='form-date'>
+          <div className='form-group form-group-day'>
+            <label className='form-label' for='dob-day'>Day</label>
+            <input className='form-control' id='dob-day' name='dob-day' type='number' pattern='[0-9]*'>
+          </div>
+          <div className='form-group form-group-month'>
+            <label className='form-label' for='dob-month'>Month</label>
+            <input className='form-control' id='dob-month' name='dob-month' type='number' pattern='[0-9]*'>
+          </div>
+          <div className='form-group form-group-year'>
+            <label className='form-label' for='dob-year'>Year</label>
+            <input className='form-control' id='dob-year' name='dob-year' type='number' pattern='[0-9]*'>
+          </div>
+        </div>
+      <fieldset>
     </div>
   )
 }
