a678d986461f3f101eeee0d9a35dcb066c045184
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-03-27 14:05:09 +0100

    A spot of linting

--- app/shared/components/Spinner/component.jsx
+++ app/shared/components/Spinner/component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Spinner {
+const Spinner = () => {
   return (
     <div className={'spinner ' + props.modifiers}>
       <span className='spinner-inner'>Loading</span>
