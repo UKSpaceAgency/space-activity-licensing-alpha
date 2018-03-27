2e52a9488bdf6cef0c3f223add55cd0e87a20677
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-03-27 13:04:29 +0100

    Updates

--- app/shared/components/PageGranular/component.jsx
+++ app/shared/components/PageGranular/component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Masthead from '../Masthead/component.jsx'
 import Footer from '../Footer/component.jsx'
 import Breadcrumb from '../Breadcrumb/component.jsx'
+import Button from '../Button/component.jsx'
 import Hero from '../Hero/component.jsx'
 import Grid from '../Grid/component.jsx'
 import GridCol from '../GridCol/component.jsx'
@@ -23,6 +24,7 @@ const PageGranular = props => {
             </GridCol>
             <GridCol className='column-two-thirds'>
               <ContentRepeaterContainer {...props}/>
+              <Button>Complete</Button>
             </GridCol>
           </Grid>
         </main>
--- app/shared/containers/SidenavContainer/component.jsx
+++ app/shared/containers/SidenavContainer/component.jsx
@@ -7,16 +7,18 @@ const SidenavContainer = props => {
   // quick hygiene thing to ensure React hasn't done something weird
   let flattened = isArray(props.content) ? props.content : Object.values(props.content)
   let list = flattened.map((v, i) => {
+
     return {
       'text': v.title.text,
-      'url': '#' + v.permalink
+      'url': '#' + v.permalink,
+      'modifiers': v.title.update ? 'update' : null
     }
   })
 
   return (
     <div className={props.modifiers || ''}>
       {props.sideNavHeading && <Heading {...props.sideNavHeading}/>}
-      <List list={list} className='spacing-top--single font-xsmall'/>
+      <List list={list} className='spacing-top--single font-xsmall spaced'/>
     </div>
   )
 }
