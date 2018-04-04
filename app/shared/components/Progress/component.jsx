7b114de2384c92a06bfe1543e5d06f068d302733
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-04 11:07:41 +0100

    Adding the section heading

--- app/shared/components/Progress/component.jsx
+++ app/shared/components/Progress/component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProgressItem from '../ProgressItem/component.jsx'
+import Heading from '../Heading/component.jsx'
 
 const Progress = props => {
   let items = props.content.map((v, i) => {
@@ -8,6 +9,7 @@ const Progress = props => {
 
   return (
     <section className='progress'>
+      <Heading {...props.heading}/>
       <ul className='progress__items  grid-row'>
         {items}
       </ul>
