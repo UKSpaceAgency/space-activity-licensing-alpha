4abeba7ef6a2f4a4c5b5976d23e42fcd1c55a7ad
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-05 20:16:56 +0100

    Linting to remove line breaks

--- app/shared/components/Tab/component.jsx
+++ app/shared/components/Tab/component.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Tab = props => {
-
   return (
     <li role='presentation' className='tabs__item'>
       <a className='tabs__link' id={props.id + '-tab'} href={'#' + props.id} role='tab' aria-controls={props.id}>{props.children}{props.label}<span className='close' data-close-tab>&times;</span></a>
