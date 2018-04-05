e35cc691b486bb427fc8f42961a406e65dc1d1eb
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-05 17:05:55 +0100

    Removing the tags / multi select

--- static/ui/js/application.js
+++ static/ui/js/application.js
@@ -3,21 +3,19 @@
 
 $(document).ready(function () {
 
-  var selectAssign = $('[data-assign]');
+  // var selectAssign = $('[data-assign]');
   var commentTarget = $('[data-comment]');
 
   // loop through target items and apply the plugin function
-  selectAssign.each(function(i, target) {
-    var tag = new Tag(target);
-    tag.init();
-  });
+  // selectAssign.each(function(i, target) {
+  //   var tag = new Tag(target);
+  //   tag.init();
+  // });
 
   if (commentTarget) {
-    var activeCommentBlocks = [];
 
     commentTarget.each(function(i, target) {
       var comment = new Comment(target);
-      activeCommentBlocks.push(comment);
       comment.init();
     })
   }
