4de73eb3343f62565306dd99ce0115a127fb4150
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-05 18:54:51 +0100

    Updating to include assignee and deadline in comments

--- app/shared/components/Comment/component.jsx
+++ app/shared/components/Comment/component.jsx
@@ -3,6 +3,9 @@ import classNames from 'classnames'
 import Longform from '../Longform/component.jsx'
 import List from '../List/component.jsx'
 import Byline from '../Byline/component.jsx'
+import Heading from '../Heading/component.jsx'
+import Grid from '../Grid/component.jsx'
+import GridCol from '../GridCol/component.jsx'
 
 const Comment = props => {
   let classes = classNames('box box--padded has-byline', props.className, props.modifiers)
@@ -11,7 +14,8 @@ const Comment = props => {
     <article className={classes} id={props.id} data-comment-block>
       <Byline {...props.byline}/>
       <Longform {...props}/>
-      {props.documents && <div className='comment-attachments'><p><strong>Comment attachments</strong></p><List list={props.documents} className='list-inline'/></div>}
+      <Grid>{props.deadline && <GridCol className='column-half'><Heading {...props.deadline}/></GridCol>}{props.assignees && <GridCol className='column-half'><Heading {...props.assignees}/></GridCol>}</Grid>
+      <Grid className='spacing-top--single'>{props.documents && <GridCol className='column-full'><p>Comment attachments</p><List list={props.documents} /></GridCol>}</Grid>
     </article>
   )
 }
