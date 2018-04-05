8aa792727919e23824cc0f41544c16bf43860f95
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-05 19:00:38 +0100

    Linting to remove line blocks

--- app/shared/components/TabPanel/component.jsx
+++ app/shared/components/TabPanel/component.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Tabpanel = props => {
-
   return (
     <section id={props.id} role='tabpanel' aria-labelledby={props.id + '-tab'} hidden>
       {props.children}
--- app/shared/components/Tablist/component.jsx
+++ app/shared/components/Tablist/component.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Tablist = props => {
-
   return (
     <ul role='tablist' className='tabs'>
       {props.children}
