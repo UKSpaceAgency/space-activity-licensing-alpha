6f9b1b9b8a00a5a215ec348ff3903f4e6dd874af
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-05 20:43:49 +0100

    Adding tabs back in

--- app/shared/components/ContentRepeater/component.jsx
+++ app/shared/components/ContentRepeater/component.jsx
@@ -5,6 +5,9 @@ import Heading from '../Heading/component.jsx'
 import Divider from '../Divider/component.jsx'
 import List from '../List/component.jsx'
 import CommentModule from '../CommentModule/component.jsx'
+import TabPanel from '../TabPanel/component.jsx'
+import Tablist from '../Tablist/component.jsx'
+import Tab from '../Tab/component.jsx'
 
 const ContentRepeater = props => {
   let classes = classNames(props.className, props.modifiers)
@@ -14,7 +17,21 @@ const ContentRepeater = props => {
       <Heading {...props.title}/>
       <Longform {...props}/>
       <Heading {...props.heading}/>
-      {props.commentBlock && <CommentModule id={props.permalink + props.id + '-open'} {...props.commentBlock}/>}
+      {props.documents && <List list={props.documents} className='list-inline'/>}
+      <Tablist>
+        <Tab id={props.id + '-open'}>
+          Inmarsat frequency (1)
+        </Tab>
+        <Tab id={props.id + '-private'}>
+          Private notes (2)
+        </Tab>
+      </Tablist>
+      <TabPanel id={props.id + '-open'}>
+        {props.commentBlock && <CommentModule id={props.permalink + props.id + '-open'} {...props.commentBlock}/>}
+      </TabPanel>
+      <TabPanel id={props.id + '-private'}>
+        {props.commentBlock && <CommentModule id={props.permalink + props.id + '-private'} {...props.commentBlock}/>}
+      </TabPanel>
       <div className='text'>
         <a href='#' className='link-back'>Back to top</a>
       </div>
