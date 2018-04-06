eab1a538fed40e9ef93b90e588b2900a47886d09
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-06 11:59:39 +0100

    Adding an accordion element

--- app/shared/components/ContentRepeater/component.jsx
+++ app/shared/components/ContentRepeater/component.jsx
@@ -11,15 +11,15 @@ import Tab from '../Tab/component.jsx'
 
 const ContentRepeater = props => {
   let classes = classNames(props.className, props.modifiers)
+  let tabs = props.commentBlock.tabs ? <React.Fragment><Tablist>{props.commentBlock.tabs.map((v, i) => <Tab key={i} id={v.id}>{v.label}</Tab>)}</Tablist>{props.commentBlock.tabs.map((v, i) => <TabPanel key={i} id={v.id}>{props.commentBlock && <CommentModule id={props.permalink + v.id} {...props.commentBlock}/>}</TabPanel>)}</React.Fragment> : null
+  let collapsible = props.commentBlock.collapsible ? <div>{tabs}</div> : tabs
   return (
     <section className={classes} id={props.permalink}>
       <Heading {...props.title}/>
       <Longform {...props}/>
       <Heading {...props.heading}/>
       {props.documents && <List list={props.documents} className='list-inline'/>}
-      {props.commentBlock.tabs && <Tablist>{props.commentBlock.tabs.map((v, i) => <Tab key={i} id={v.id}>{v.label}</Tab>)}</Tablist>}
-      {props.commentBlock.tabs && props.commentBlock.tabs.map((v, i) => <TabPanel key={i} id={v.id}>{props.commentBlock && <CommentModule id={props.permalink + v.id} {...props.commentBlock}/>}</TabPanel>)}
-
+      {collapsible}
       <div className='text'>
         <a href='#' className='link-back'>Back to top</a>
       </div>
