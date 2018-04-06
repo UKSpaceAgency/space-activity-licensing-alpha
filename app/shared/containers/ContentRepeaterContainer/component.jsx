18ddad5295e9bccf0fa85f1a35b31148f12a93fb
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-06 09:59:31 +0100

    Changing name and adding heading title

--- app/shared/containers/ContentRepeaterContainer/component.jsx
+++ app/shared/containers/ContentRepeaterContainer/component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ContentRepeater from '../../components/ContentRepeater/component.jsx'
+import Heading from '../../components/Heading/component.jsx'
 
 const ContentRepeaterContainer = props => {
   let content = props.content.map((v, i) => {
@@ -8,6 +9,7 @@ const ContentRepeaterContainer = props => {
 
   return (
     <div className={props.modifiers || ''}>
+      {props.heading && <Heading {...props.heading}/>}
       {content}
     </div>
   )
