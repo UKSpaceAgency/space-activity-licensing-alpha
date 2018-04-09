eb6ba87d928e51070b6a02ce559c26fcadf793a9
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-09 15:52:01 +0100

    Updates and tweaks to align with Sketch files

--- app/shared/components/OverviewList/component.jsx
+++ app/shared/components/OverviewList/component.jsx
@@ -5,14 +5,14 @@ import Heading from '../Heading/component.jsx'
 import classNames from 'classnames'
 
 const OverviewList = props => {
-  let classes = classNames('bg-grey spacing-bottom--single', props.className, props.modifiers, 'relative bottom-border bottom-border-' + props.label.class)
+  let classes = classNames('bg-grey spacing-bottom--single relative bottom-border ', props.className, props.modifiers, 'bottom-border-' + props.label.class)
 
   return (
     <section className={classes}>
       <Grid>
         <GridCol className='column-half'>
           <div className='padding-medium'>
-            <a href={props.url} className='no-underline'><h3 className='heading-medium spacing-top--flush'>{props.status && <span className='badge bg-dark-grey font-xsmall badge--flush'>{props.status} </span>} {props.heading.text}</h3></a>
+            <a href={props.url} className='no-underline'><h3 className='heading-medium spacing-top--flush'>{props.status && <span className={'badge font-xsmall badge--flush ' + props.status.modifiers}>{props.status.label} </span>} {props.heading.text}</h3></a>
             {props.items && props.items.map((v, i) => <p key={i}><strong>{v.text}</strong>: {v.value}</p>)}
           </div>
         </GridCol>
@@ -25,7 +25,6 @@ const OverviewList = props => {
         <GridCol className='column-quarter'>
           <div className='padding-medium'>
             <p>Application status <br/><strong>{props.appStatus}</strong></p>
-
           </div>
         </GridCol>
       </Grid>
