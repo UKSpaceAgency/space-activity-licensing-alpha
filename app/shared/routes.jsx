a6763bea63d84137ebc079f21ffe15c592d45744
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-16 10:34:36 +0100

    New form pages

--- app/shared/routes.jsx
+++ app/shared/routes.jsx
@@ -74,6 +74,8 @@ let getRoutes = store => {
         <Route path='page-1' component={withFallback(PageContainer)} onEnter={getPage} slug='apply-page-1' />
         <Route path='page-2' component={withFallback(PageContainer)} onEnter={getPage} slug='apply-page-2' />
         <Route path='page-3' component={withFallback(PageContainer)} onEnter={getPage} slug='apply-page-3' />
+        <Route path='page-4' component={withFallback(PageContainer)} onEnter={getPage} slug='apply-page-4' />
+        <Route path='page-5' component={withFallback(PageContainer)} onEnter={getPage} slug='apply-page-5' />
       </Route>
       <Route path='*' component={withFallback(PageContainer)} onEnter={getPage} slug='no-match' />
     </Route>
