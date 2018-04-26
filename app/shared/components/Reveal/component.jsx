251bed0a863f5d4892e561c590d76c778a9cb539
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-04-26 15:18:58 +0100

    Order change and reveal panels added

--- app/shared/components/Reveal/component.jsx
+++ app/shared/components/Reveal/component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import classNames from 'classnames'
 import formBuilder from '../FormBuilder/component.jsx'
 
 const Reveal = props => {
   let Components = formBuilder(props.content)
+  let classes = classNames('js-hidden', props.className, props.modifiers)
+
   return (
-    <div className='js-hidden' id={props.id}>
+    <div className={classes} id={props.id}>
       {Components}
     </div>
   )
