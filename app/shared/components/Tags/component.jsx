c7c2dc11ec19b71802c2b9e1c6e69d8ecc3945d1
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-05-11 12:22:35 +0100

    Linting

--- app/shared/components/Tags/component.jsx
+++ app/shared/components/Tags/component.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Tags = props => {
-
   return (
     <ul className='tags'>
       {props.tags.map((v, i) => <li key={i} className='tag'><span className='tag__text'>{v.label}</span><a href='#' className='close'>&times;</a></li>)}
