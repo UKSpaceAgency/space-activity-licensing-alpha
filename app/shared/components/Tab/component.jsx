c5928bc9f0a99f6fe26eb683e872b113707b334b
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-06-18 15:45:53 +0100

    Ongoing changes

--- app/shared/components/Tab/component.jsx
+++ app/shared/components/Tab/component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Tab = props => {
   return (
     <li role='presentation' className='tabs__item'>
-      <a className='tabs__link' id={props.id + '-tab'} href={'#' + props.id} role='tab' data-tab={props.id} aria-controls={props.id}>{props.children}{props.label} <span data-tab-count></span><span className='close' data-close-tab>&times;</span></a>
+      <a className='tabs__link' id={props.id + '-tab'} href={'#' + props.id} role='tab' data-tab={props.id} aria-controls={props.id}>{props.children}{props.label} <span data-tab-count className={props.new || null}></span><span className='close' data-close-tab>&times;</span></a>
     </li>
   )
 }
