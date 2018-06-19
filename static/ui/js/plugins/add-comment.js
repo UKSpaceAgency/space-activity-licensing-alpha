55124ed35187a5eb1d7536a973a00020a37e4a17
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-06-19 10:29:33 +0100

    Cheat-striping the comments

--- static/ui/js/plugins/add-comment.js
+++ static/ui/js/plugins/add-comment.js
@@ -24,7 +24,7 @@ function Comment(element, tabs) {
     commentBlock: '[data-comment-block]',
     file: '[data-file]',
     textarea: '[data-textarea]',
-    template: '<article class="box box--flat {{extra}}" data-comment-block="true"><div class="grid-row has-byline"><div class="byline column-half spacing-bottom--single"><i class="icon icon--byline icon-account"><span class="visually-hidden">account</span></i><h3 class="heading-small"><span>{{by}}</span></h3><time class="time" datetime="2018-03-26T19:34:15+00:00">10.04am 22nd March 2018</time></div>{{#high}}<div class="column-half"><div class="notice"><i class="icon icon-warning icon-warning--red icon--large"><span class="visually-hidden">High priority</span></i><strong class="bold-small red">High priority</strong></div></div>{{/high}}</div><article>{{#subject}}<h3 class="heading-medium spacing-bottom--tight">{{subject}}</h3>{{/subject}}<div class="long-form">{{text}}</div></article><div class="grid-row"><div class="column-half"><p class="heading-small"><span><span class="quiet">Deadline</span><br>4 June 2018</span></p></div><div class="column-half"><p class="heading-small"><span><span class="quiet">Assigned to</span><br>{{assigned}}</span></p></div></div><div class="grid-row spacing-top--single"><div class="column-full">{{#file}}<p><strong>Comment attachments</strong></p><p><a href="/pdfs/disposal-discovery.pdf">{{file}}</a></p>{{/file}}</div></div></article>'
+    template: '<article class="box box--flat" data-comment-block="true"><div class="grid-row has-byline"><div class="byline column-half spacing-bottom--single"><i class="icon icon--byline icon-account"><span class="visually-hidden">account</span></i><h3 class="heading-small"><span>{{by}}</span></h3><time class="time" datetime="2018-03-26T19:34:15+00:00">10.04am 22nd March 2018</time></div>{{#high}}<div class="column-half"><div class="notice"><i class="icon icon-warning icon-warning--red icon--large"><span class="visually-hidden">High priority</span></i><strong class="bold-small red">High priority</strong></div></div>{{/high}}</div><article>{{#subject}}<h3 class="heading-medium spacing-bottom--tight">{{subject}}</h3>{{/subject}}<div class="long-form">{{text}}</div></article><div class="grid-row"><div class="column-half"><p class="heading-small"><span><span class="quiet">Deadline</span><br>4 June 2018</span></p></div><div class="column-half"><p class="heading-small"><span><span class="quiet">Assigned to</span><br>{{assigned}}</span></p></div></div><div class="grid-row spacing-top--single"><div class="column-full">{{#file}}<p><strong>Comment attachments</strong></p><p><a href="/pdfs/disposal-discovery.pdf">{{file}}</a></p>{{/file}}</div></div></article>'
   }
 
 
@@ -113,7 +113,7 @@ function Comment(element, tabs) {
       by: person || 'Admin',
       subject: el.find(options.subject).val() || null,
       high: checked(),
-      extra: person === 'Andy Assessor' ? '' : 'comment-admin',
+      // extra: person === 'Andy Assessor' ? '' : 'comment-admin',
       assigned: sel !== '' ? sel : 'No action'
     }
 
