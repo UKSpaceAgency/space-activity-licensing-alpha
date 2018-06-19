953e83b4e099f0d0b4d94d72fe9fa31557245964
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-06-19 11:52:52 +0100

    Adding new flag to comment

--- app/shared/components/Comment/component.jsx
+++ app/shared/components/Comment/component.jsx
@@ -13,6 +13,7 @@ const Comment = props => {
   return (
     <article className={classes} id={props.id} data-comment-block>
       <Byline {...props.byline}/>
+      {props.subject && <Heading {...props.subject}/>}
       <Longform {...props}/>
       <Grid>{props.deadline && <GridCol className='column-half'><Heading {...props.deadline}/></GridCol>}{props.assignees && <GridCol className='column-half'><Heading {...props.assignees}/></GridCol>}</Grid>
       <Grid className='spacing-top--single'>{props.documents && <GridCol className='column-full'><p>Comment attachments</p><List list={props.documents} /></GridCol>}</Grid>
