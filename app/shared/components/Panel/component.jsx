67da8b86daa4e96558c489c15fe0c9fc34d9015a
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-07-24 11:48:39 +0100

    Content changes and updates to generate license

--- app/shared/components/Panel/component.jsx
+++ app/shared/components/Panel/component.jsx
@@ -3,9 +3,10 @@ import classNames from 'classnames'
 
 const Panel = props => {
   let classes = classNames('panel', props.className, props.modifiers)
+  let id = props.id ? {id: props.id} : null
 
   return (
-    <div className={classes}>
+    <div className={classes} {...id}>
       {props.children}
       {props.content && <div className='text' dangerouslySetInnerHTML={{__html: props.content}} />}
     </div>
